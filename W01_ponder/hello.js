e9ae00feed920a571556a72d5d28d5884b423c45
Gakyeong <bae21006@byui.edu>
2024-09-19 00:59:47 +0900

    class activity1

---
+++ W01_ponder/hello.js
@@ -0,0 +1,9 @@
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+console.log("total:", total);
+// getelemenbyID : grap in html
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
+// breakpoint - browser source 
